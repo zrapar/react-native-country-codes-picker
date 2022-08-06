880b20342c6c61b5fc3b51e750f6492a81b2716c
Rafael Zerpa <zrapar@gmail.com>
2022-08-06 16:27:08 -0500

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import CountryPicker from "./components/CountryPicker";
 import { countryCodes } from "./constants/countryCodes";
 
 export const getCountryCodes = () => countryCodes;
-export const findCountry = (code = "") => {
+export const findCountryByCode = (code = "") => {
   if (code && code.length !== "") {
     const country = countryCodes.find(
       (country) => country.code.toLowerCase() === code.toLowerCase().trim()
@@ -12,5 +12,31 @@ export const findCountry = (code = "") => {
     }
   }
   return null;
+};
+export const findCountryByDialCode = (dialCode = "") => {
+  if (dialCode && dialCode.length !== "") {
+    const country = countryCodes.find(
+      (country) =>
+        country.dial_code.toLowerCase() === dialCode.toLowerCase().trim()
+    );
+    if (country) {
+      return country;
+    }
+  }
+  return null;
+};
+export const findCountryByName = (name = "") => {
+  let country;
+  if (name && name.length !== "") {
+    for (const cntry of countryCodes) {
+      const { name: nameCountry } = cntry;
+      Object.keys(nameCountry).forEach((n) => {
+        if (nameCountry[n].includes(name)) {
+          country = cntry;
+        }
+      });
+    }
+  }
+  return country;
 };
 export default CountryPicker;
