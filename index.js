e366c79596e464b53c2d27310ac96065e4a4d24f
Rafael Zerpa <zrapar@gmail.com>
2022-08-06 16:14:15 -0500

    update

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 import CountryPicker from "./components/CountryPicker";
 import { countryCodes } from "./constants/countryCodes";
 
-export const countryCodes = countryCodes;
+export const getCountryCodes = () => countryCodes;
+export const findCountry = (code = "") => {
+  if (code && code.length !== "") {
+    const country = countryCodes.find(
+      (country) => country.code.toLowerCase() === code.toLowerCase().trim()
+    );
+    if (country) {
+      return country;
+    }
+  }
+  return null;
+};
 export default CountryPicker;
